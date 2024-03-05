b63f8227289c1d7e6bede66216926840e91a9b76
bluesillybeard <bluesillybeard2@gmail.com>
2024-03-04 20:59:45 -0700

    fruit can be cut now

--- src/main.zig
+++ src/main.zig
@@ -29,6 +29,8 @@ const FruitComponent = struct {
 const Fruit = struct {
     xPos: f32,
     yPos: f32,
+    lastXPos: f32,
+    lastYPos: f32,
     angle: f32,
     xVel: f32,
     yVel: f32,
@@ -64,6 +66,13 @@ const AcerolaGameJamSystem = struct {
             .bgUniforms = undefined,
             .fgEntity = undefined,
             .fgUniforms = undefined,
+            .cursorX = 0,
+            .cursorY = 0,
+            .lastCursorX = 0,
+            .lastCursorY = 0,
+            .lastCursorUpdate = 0,
+            .cursorVelX = 0,
+            .cursorVelY = 0,
         };
     }
 
@@ -112,8 +121,9 @@ const AcerolaGameJamSystem = struct {
         this.fgUniforms[0] = .{.mat4 = zrender.Mat4.identity};
         this.fgUniforms[1] = .{.texture = this.fgTexture};
 
-        renderSystem.onUpdate.sink().connectBound(this, "update");
+        renderSystem.onFrame.sink().connectBound(this, "onFrame");
         renderSystem.onMousePress.sink().connectBound(this, "onClick");
+        renderSystem.onMouseMove.sink().connectBound(this, "onMouseMove");
         _ = settings;
     }
 
@@ -131,7 +141,7 @@ const AcerolaGameJamSystem = struct {
         });
     }
 
-    pub fn update(this: *@This(), args: zrender.OnUpdateEventArgs) void {
+    pub fn onFrame(this: *@This(), args: zrender.OnFrameEventArgs) void {
         this.timeSinceStart += args.delta;
         this.fruitSpawnCountown -= args.delta;
         var random = this.rand.random();
@@ -143,33 +153,20 @@ const AcerolaGameJamSystem = struct {
             this.spawnFruit(args.registries, this.fruitTexture, Fruit {
                 .angle = 0,
                 .aVel = random.float(f32) * 1.5 - (1.5 / 2.0),
-                .scale = 0.15,
+                .scale = 0.07,
                 .xPos = random.float(f32) * 2 - 1,
                 .yPos = -1.5,
+                // These are only used for detecting when the fruit is cut so it doesn't matter they don't match
+                .lastXPos = 0,
+                .lastYPos = -1.5,
                 // TODO: bias this towards the middle of the screen
                 .xVel = random.float(f32) * 1.0 - (1.0 / 2.0),
                 .yVel = 3,
             });
             this.fruitSpawnCountown += 1 * std.time.us_per_s;
         }
-        
-        
-        // TODO: define camera parameters elsewhere
-        const resolution = renderSystem.getWindowResolution();
-        // Gotta say, Zig's float-int casting is such a pain
-        const aspect: f32 = @as(f32, @floatFromInt(resolution.height)) / @as(f32, @floatFromInt(resolution.width));
-        const cameraCenter = zlm.vec2(0, 0);
-        
-        var cameraRadiusX: f32 = 1;
-        var cameraRadiusY: f32 = cameraRadiusX * aspect;
-        if(resolution.width > resolution.height) {
-            cameraRadiusY = 1;
-            cameraRadiusX = cameraRadiusY / aspect;
-        }
         // camera transformations
-        var cameraTransform = zlm.Mat4.identity;
-        cameraTransform = zlm.Mat4.createOrthogonal(-cameraRadiusX, cameraRadiusX, -cameraRadiusY, cameraRadiusY, 0.01, 10).mul(cameraTransform);
-        cameraTransform = zlm.Mat4.createLook(zlm.Vec3{ .x = cameraCenter.x, .y = cameraCenter.y, .z = 0.5 }, zlm.Vec3.unitZ, zlm.Vec3.unitY).mul(cameraTransform);
+        const cameraTransform = getCameraTransform(renderSystem.getWindowResolution());
         const view = args.registries.globalEcsRegistry.basicView(FruitComponent);
         var iterator = view.entityIterator();
         var fruitToRemove = std.ArrayList(ecs.Entity).init(this.allocator);
@@ -185,9 +182,20 @@ const AcerolaGameJamSystem = struct {
             const renderComponent = args.registries.globalEcsRegistry.get(zrender.RenderComponent, entity);
             // Update the fruit
             fruit.fruit.yVel += gravity * deltaSeconds;
+            fruit.fruit.lastXPos = fruit.fruit.xPos;
+            fruit.fruit.lastYPos = fruit.fruit.yPos;
             fruit.fruit.yPos += fruit.fruit.yVel * deltaSeconds;
             fruit.fruit.xPos += fruit.fruit.xVel * deltaSeconds;
             fruit.fruit.angle += fruit.fruit.aVel * deltaSeconds;
+
+            // If the fruit is near the cursor and the cursor is moving greater than a certain speed,
+            // then that fruit gets deleted
+            const fruitCursorDistance = segmentSegmentDistance(this.cursorX, this.cursorY, this.lastCursorX, this.lastCursorY, fruit.fruit.xPos, fruit.fruit.yPos, fruit.fruit.lastXPos, fruit.fruit.lastYPos);
+            const cursorVelocity = this.cursorVelX * this.cursorVelX + this.cursorVelY * this.cursorVelY;
+            if(fruitCursorDistance < fruit.fruit.scale and cursorVelocity > 0.2) {
+                fruitToRemove.append(entity) catch @panic("Out of memory!");
+                continue;
+            }
 
             // Update the render component with the new fruit data
             const objectTransform = getFruitTransform(fruit.fruit);
@@ -216,7 +224,88 @@ const AcerolaGameJamSystem = struct {
         transform = zlm.Mat4.createUniformScale(fruit.scale).mul(transform);
         
         return transform;
+    }
 
+    fn getCameraTransform(resolution: anytype) zlm.Mat4 {
+        // Gotta say, Zig's float-int casting is such a pain
+        const aspect: f32 = @as(f32, @floatFromInt(resolution.height)) / @as(f32, @floatFromInt(resolution.width));
+        const cameraCenter = zlm.vec2(0, 0);
+        
+        var cameraRadiusX: f32 = 1;
+        var cameraRadiusY: f32 = cameraRadiusX * aspect;
+        if(resolution.width > resolution.height) {
+            cameraRadiusY = 1;
+            cameraRadiusX = cameraRadiusY / aspect;
+        }
+        var cameraTransform = zlm.Mat4.identity;
+        cameraTransform = zlm.Mat4.createOrthogonal(-cameraRadiusX, cameraRadiusX, -cameraRadiusY, cameraRadiusY, 0.01, 10).mul(cameraTransform);
+        cameraTransform = zlm.Mat4.createLook(zlm.Vec3{ .x = cameraCenter.x, .y = cameraCenter.y, .z = 0.5 }, zlm.Vec3.unitZ, zlm.Vec3.unitY).mul(cameraTransform);
+        return cameraTransform;
+    }
+
+    // Returns the euclidian distance betwen the line defined by (p1x, p1y) (p2x, p2y), and the point (x, y)
+    fn segmentPointDistance(x1: f32, y1: f32, x2: f32, y2: f32, x: f32, y: f32) f32 {
+        const A = x - x1;
+        const B = y - y1;
+        const C = x2 - x1;
+        const D = y2 - y1;
+
+        const dot = A * C + B * D;
+        const len_sq = C * C + D * D;
+        var param: f32 = -1;
+        if (len_sq != 0) //in case of 0 length line
+            param = dot / len_sq;
+
+        var xx: f32 = undefined;
+        var yy: f32 = undefined;
+
+        if (param < 0) {
+            xx = x1;
+            yy = y1;
+        }
+        else if (param > 1) {
+            xx = x2;
+            yy = y2;
+        }
+        else {
+            xx = x1 + param * C;
+            yy = y1 + param * D;
+        }
+
+        const dx = x - xx;
+        const dy = y - yy;
+        return @sqrt(dx * dx + dy * dy);
+    }
+
+    /// Returns the distance between two line segments
+    /// line 1 is (x11, y11) (x12, y12) and line 2 is (x21, y21) (x22, y22)
+    fn segmentSegmentDistance(x11: f32, y11: f32, x12: f32, y12: f32, x21: f32, y21: f32, x22: f32, y22: f32) f32 {
+        // if they intersect, the distance is zero
+        if(segmentsIntersect(x11, y11, x12, y12, x21, y21, x22, y22)){
+            return 0.0;
+        }
+        var distance = segmentPointDistance(x21, y21, x22, y22, x11, y11);
+        // Min of 4 distances from segments to points
+        distance = @min(segmentPointDistance(x21, y21, x22, y22, x12, y12), distance);
+        distance = @min(segmentPointDistance(x11, y11, x12, y12, x21, y21), distance);
+        distance = @min(segmentPointDistance(x11, y11, x12, y12, x22, y22), distance);
+        return distance;
+    }
+    /// Returns true if two segments intersect, falst otherwise.
+    /// line 1 is (x11, y11) (x12, y12) and line 2 is (x21, y21) (x22, y22)
+    fn segmentsIntersect(x11: f32, y11: f32, x12: f32, y12: f32, x21: f32, y21: f32, x22: f32, y22: f32) bool {
+        const dx1 = x12 - x11;
+        const dy1 = y12 - y11;
+        const dx2 = x22 - x21;
+        const dy2 = y22 - y21;
+        const delta = dx2 * dy1 - dy2 * dx1;
+        if(delta == 0) return false;
+        const s = (dx1 * (y21 - y11) + dy1 * (x11 - x21)) / delta;
+        const t = (dx2 * (y11 - y21) + dy2 * (x21 - x11)) / (-delta);
+        return (0 <= s and s <= 1) and (0 <= t and t <= 1);
+    }
+    fn pointPointdistance(x1: f32, y1: f32, x2: f32, y2: f32) f32 {
+        return @sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
     }
 
     pub fn onClick(this: *@This(), args: zrender.OnMousePressEventArgs) void {
@@ -224,6 +313,27 @@ const AcerolaGameJamSystem = struct {
         _ = this;
     }
 
+    pub fn onMouseMove(this: *@This(), args: zrender.OnMouseMoveEventArgs) void {
+            const cursorDeltaTimeMicros: f32 = @floatFromInt(args.time - this.lastCursorUpdate);
+        const cursorDeltaTime = cursorDeltaTimeMicros / std.time.us_per_s;
+        // If the delta is too small, then then ignore this movement
+        if(cursorDeltaTimeMicros < 1000) return;
+        // vec4 so it can be transformed by the camera
+        const posInPixels = zlm.Vec4{.x = @floatFromInt(args.x), .y = @floatFromInt(args.y), .z = 0, .w = 1};
+        const renderSystem = args.registries.globalRegistry.getRegister(zrender.ZRenderSystem).?;
+        const resolution = renderSystem.getWindowResolution();
+        // First, convert pixels to screen space
+        const posInScreen = posInPixels.div(zlm.Vec4.new(@floatFromInt(resolution.width), @floatFromInt(resolution.height), 1, 1)).sub(zlm.Vec4.new(0.5, 0.5, 0, 0)).mul(zlm.Vec4.new(2, -2, 1, 1));
+        const inverseCameraMatrix = getCameraTransform(resolution).invert() orelse unreachable;
+        const posInWorld = posInScreen.transform(inverseCameraMatrix);
+        this.lastCursorUpdate = args.time;
+        this.lastCursorX = this.cursorX;
+        this.lastCursorY = this.cursorY;
+        this.cursorX = posInWorld.x;
+        this.cursorY = posInWorld.y;
+        this.cursorVelX = (this.cursorX - this.lastCursorX) / cursorDeltaTime;
+        this.cursorVelY = (this.cursorY - this.lastCursorY) / cursorDeltaTime;
+    }
     pub fn systemDeinitGlobal(this: *@This(), registries: *zengine.RegistrySet) void {
         _ = registries;
         _ = this;
@@ -276,6 +386,16 @@ const AcerolaGameJamSystem = struct {
     bgUniforms: [2]zrender.Uniform,
     fgEntity: ecs.Entity,
     fgUniforms: [2]zrender.Uniform,
+    // Cursor position in world space.
+    cursorX: f32,
+    cursorY: f32,
+    lastCursorX: f32,
+    lastCursorY: f32,
+    // the frame time when the cursor was last updated
+    lastCursorUpdate: i64,
+    // Cursor speed in world space / second
+    cursorVelX: f32,
+    cursorVelY: f32,
 };
 
 pub fn main() !void {
